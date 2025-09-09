5356d9c3d5063ee1f229bd7ba9ae63c04bb27988
preetnagda <preetnagda34@gmail.com>
2025-09-09 16:39:16 +1000

    keymap: delete all buffers

--- init.lua
+++ init.lua
@@ -93,6 +93,8 @@ vim.o.confirm = true
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+-- Clear all buffers
+vim.keymap.set('n', '<leader>bd', '<cmd>%bd|e#<CR>', { desc = 'Close [B]uffer [D]o all' })
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
